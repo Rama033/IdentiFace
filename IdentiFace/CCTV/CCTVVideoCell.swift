bed68ca5f838d8562f40440dbe8ac71759f02482
CE-MAC-23 <ce-mac-23@ce-mac-23ui-macbook-pro.local>
2018-08-04 18:48:53 +0900

    영상 재생 o 사용자 관리 x 설정 x

---
+++ IdentiFace/CCTV/CCTVVideoCell.swift
@@ -0,0 +1,26 @@
+//
+//  CCTVVideoCell.swift
+//  IdentiFace
+//
+//  Created by CE-MAC-23 on 2018. 8. 4..
+//  Copyright © 2018년 CE-MAC-23. All rights reserved.
+//
+
+import UIKit
+
+class CCTVVideoCell: UITableViewCell {
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var dayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ IdentiFace/UserData.swift
@@ -0,0 +1,9 @@
+//
+//  UserData.swift
+//  IdentiFace
+//
+//  Created by CE-MAC-23 on 2018. 8. 4..
+//  Copyright © 2018년 CE-MAC-23. All rights reserved.
+//
+
+import Foundation
